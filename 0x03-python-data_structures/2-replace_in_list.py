fdc1b0ffa4d4d63d4f003e50a4fdb31f8eda61cd
gedoman <gedoman90@gmail.com>
2024-03-19 15:14:01 -0700

    This function adds an item to the list at a specific location

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""This function adds an item to the list at
+a specific location"""
 def replace_in_list(my_list, idx, element):
+    """faunction that replace an element
+      my_list: list to replace its element
+      idx: int the index of element replace
+      element: Any the new item
+    """
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
         my_new_list=my_list
         my_new_list[idx]=element
         return my_new_list
-        return my_list
