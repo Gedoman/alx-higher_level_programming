b0cb704b35f525a49dd888dddb502de1af2d8b1f
Gedoman <gedoman90@gmail.com>
2024-03-20 17:43:59 -0700

    update

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if a_dictionary.get(key) is not None:
-        del a_dictionary[key]
-    return a_dictionary
+    if a_dictionary:
+        try:
+                del a_dictionary[key]
+        except KeyError:
+                pass
+        return a_dictionary
