d8e9cbf0bb5a7cc50a6c2e6de3950077c3fc727c
Gedoman <gedoman90@gmail.com>
2024-03-20 17:22:22 -0700

    update

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""A function that calculates the average
+of the elements and returns it"""
+
+
 def weight_average(my_list=[]):
     if not my_list:
         return 0
     else:
-        return(sum(a * b for a, b in my_list) / sum(b for a, b in my_list))
+        return(sum(a * b for a, b in my_list) / sum(b for a, b in my_list)i
