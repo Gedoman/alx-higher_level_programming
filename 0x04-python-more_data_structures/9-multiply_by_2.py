14f8d2bd5567319e564018e40ee83b16790c3335
Gedoman <gedoman90@gmail.com>
2024-03-20 17:59:46 -0700

    update

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+
 def multiply_by_2(a_dictionary):
-    return{k: v * 2 for k, v in a_dictionary.items()}
+    new_dec = {}
+    if a_dictionary:
+        for k, v in a_dictionary.items():
+                new_dec.update({k: v * 2})
+    return new_dec
