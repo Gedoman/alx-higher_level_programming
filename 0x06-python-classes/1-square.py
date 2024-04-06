17a410f5074d8814e0b5de8d4dd06800e79cce34
gedoman <gedoman90@gmail.com>
2024-04-06 15:49:52 -0700

    this class has receive the size of square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""modual of class square"""
+
+
+class Square:
+    """Define a Square"""
+    def __init__(self, ize):
+        """Constructor.
+        Parameters:
+        ----------
+        size : int
+            Length of a side of the square.
+        ----------
+        """
+        self.__size = size
