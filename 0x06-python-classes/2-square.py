e00a49ae571f4dc6bb2153d626e703b555c5244b
gedoman <gedoman90@gmail.com>
2024-04-06 17:11:01 -0700

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Square Modual"""
+
+class Square:
+    """Define a Square."""
+
+    def __inti__(self, size=0):
+        """ Constructor.
+
+        Arge:
+            size : int
+            length od a side of the square.
+
+        raises:
+            TypeError : if size is not an integer
+            ValueError : if size is less than zero
+        """
+        if size not in(int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+
