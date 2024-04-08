cd1ee34054750af022829edd248998b1e484f08f
gedoman <gedoman90@gmail.com>
2024-04-07 20:49:43 -0700

    Print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
-"""Modual fro Print square method"""
+"""Modual for Print square method"""
 
 
 def print_square(size):
+    """Method for printing a square.
+    Args:
+        size: integer
+            The int size of the square's side.
+    Raises:
+        TypeError: If size is not an int.
+        ValueError: If size is < zero.
     """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    print((("#" * size + "\n") * size), end="")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
