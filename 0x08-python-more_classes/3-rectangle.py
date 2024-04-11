4db5e4e05bb6518715b1da4f7828cbbcc8283ef2
gedoman <gedoman90@gmail.com>
2024-04-11 03:13:32 +0200

    String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -3,12 +3,14 @@
 define a class Rectangle
 """
 
+
 class Rectangle:
     """Representtation of a rectangle"""
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
         self.height = height
         self.width = width
+
     @property
     def width(self):
         """getter for the private instance attribute width"""
