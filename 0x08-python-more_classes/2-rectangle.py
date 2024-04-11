17ed2b4291b2a6487689e0f98988bad21921bf2e
gedoman <gedoman90@gmail.com>
2024-04-11 02:40:50 +0200

    Area and Perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -38,9 +38,11 @@ class Rectangle:
         if value < 0:
             raise TypeError("height must be >= 0")
         self.__height = value
+
     def area(self):
         """returns the area of the rectangle"""
         return self.__width * self.height
+
     def perimeter(self):
         """return the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
