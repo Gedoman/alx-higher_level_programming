cc8e46aad40c121c9581786d845864f08b20d761
gedoman <gedoman90@gmail.com>
2024-04-16 01:59:27 +0200

    Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,6 +2,6 @@
 """module for is_kind_of_class mithod"""
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """determins if an object is a subclass of a class"""
     return isinstance(obj, a_class) and type(obj) != a_class
