34d48069b5391cfae1e14186557dbee99751f0fd
gedoman <gedoman90@gmail.com>
2024-04-16 01:47:17 +0200

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module for is_kind_of_class mithod"""
+
+
+def is_kind_of_class(obj, a_class):
+    """determins if an object is a subclass of a class"""
+    return isinstance(obj, a_class)
