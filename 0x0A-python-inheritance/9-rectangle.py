3753e7ce0a81551d2f2006858febb207f05acb03
gedoman <gedoman90@gmail.com>
2024-04-16 04:12:58 +0200

    Full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""module for Rectangle class"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """a subclass representing a rectangle"""
+    def __init__(self, width, height):
+        """constractor"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+    def area(self):
+        """method which return area of retangle"""
+        return self.__width * self__height
+
+    def __str__(self):
+        """string representation method"""
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__heigth)
