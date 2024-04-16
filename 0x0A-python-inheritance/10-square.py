ea8c0dff8d34d891db6cf4d70f0e30b36c7e6e17
gedoman <gedoman90@gmail.com>
2024-04-16 04:46:00 +0200

    Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module for Rectangle class"""
+Rectangle = __import__("9-base_geometry").Rectangle
+
+
+class Square(Rectangle):
+    """a subclass representing a rectangle"""
+    def __init__(self, size):
+        """constractor"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size_
+
+    def area(self):
+        """method which return area of retangle"""
+        return self.__size ** 2
