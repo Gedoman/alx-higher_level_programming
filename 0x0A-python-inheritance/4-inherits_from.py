29fb83a1b4ba8b2f8cdefe6a935484db4e906392
gedoman <gedoman90@gmail.com>
2024-04-17 00:04:16 +0200

    updat the file and use isinstance() func

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """determins if an object is a subclass of a class"""
-    return (not isinstance(obj, a_class)) and isinstance(obj, a_class)
+    return type(obj) != a_class and isinstance(obj, a_class)
