476d297f71632856eb70ee3f680ce43c9c3fb167
gedoman <gedoman90@gmail.com>
2024-04-16 22:21:01 +0200

    wirte file func to write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" module for write_file method"""
+
+
+def write_file(filename="", text=""):
+    """writ on the file
+    Args:
+        filename: file
+        text:text will me added in the file
+    """
+    with open(filename,encoding="utf-8") as file:
+        file.write('This School is so cool!\n')
